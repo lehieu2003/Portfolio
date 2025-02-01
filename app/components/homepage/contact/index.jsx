f64b02959befb91b97321a219fd065d0a25153ed
hieu <lehieunghiahanh76@gmail.com>
2025-02-01 22:49:18 +0700

    clear capcha

--- app/components/homepage/contact/index.jsx
+++ app/components/homepage/contact/index.jsx
@@ -6,7 +6,6 @@ import { CiLocationOn } from "react-icons/ci";
 import { FaFacebook } from "react-icons/fa";
 import { IoLogoGithub, IoMdCall } from "react-icons/io";
 import { MdAlternateEmail } from "react-icons/md";
-import ContactWithCaptcha from "./contact-with-captcha";
 import ContactWithoutCaptcha from "./contact-without-captcha";
 import Image from "next/image";
 
@@ -27,12 +26,8 @@ function ContactSection() {
         <span className="h-36 w-[2px] bg-[#1a1443]"></span>
       </div>
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-16 items-center">
-        {/* {process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY &&
-        process.env.NEXT_PUBLIC_RECAPTCHA_SECRET_KEY ? (
-          <ContactWithCaptcha />
-        ) : ( */}
+        
         <ContactWithoutCaptcha />
-        {/* )} */}
 
         <div className="lg:w-3/4 ">
           <div className="flex flex-col gap-5 lg:gap-9">
