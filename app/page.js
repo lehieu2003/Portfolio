ebc55c1054dbb0ebb6e78787b41427584a5e329b
hieu <lehieunghiahanh76@gmail.com>
2025-02-03 09:07:15 +0700

    fix

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
 
-export default async function Home() {
+export default function Home() {
   return (
     <>
       <HeroSection />
