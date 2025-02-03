26a5b163ff7bb25de12f50c23f9f9a6e2c529bd3
hieu <lehieunghiahanh76@gmail.com>
2025-02-03 10:32:42 +0700

    update cv

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -3,7 +3,7 @@ export const personalData = {
   profile: "/avartar1.png",
   designation: "Software Developer",
   description:
-    "My name is Hieu, and I am a third-year Software Engineering student at International University. I have hands-on experience building web applications using ReactJS, React Native, NodeJS, and Java. With my knowledge of UI/UX design, I enjoy solving front-end problems and creating the best user experience for users. I am also committed to learning new technologies and best practices to become a better engineer.",
+    "My name is Hieu, and I am a final-year Software Engineering student at International University. I have hands-on experience building web applications using ReactJS, React Native, NodeJS, and Java. With my knowledge of UI/UX design, I enjoy solving front-end problems and creating the best user experience for users. I am also committed to learning new technologies and best practices to become a better engineer.",
   email: "lehieunghiahanh76@gmail.com",
   phone: "0868296322",
   address: "Thu Duc, Ho Chi Minh City, Vietnam",
@@ -11,5 +11,5 @@ export const personalData = {
   facebook: "https://www.facebook.com/profile.php?id=100049807973142",
   linkedIn: "https://www.linkedin.com/in/hieu-le-202859311/",
   resume:
-    "https://drive.google.com/file/d/10qb_Wh2vuD1f8S0B51Iob-31lmZX7D1Q/view?usp=sharing",
+    "https://drive.google.com/drive/folders/1kkKToraG3-S90UThyLbjY4fobpO4L4Pn?fbclid=IwAR23yPWGbn9SrAdxa6RJx5zilX8FMsBiCAck9shA8KUpEI8zRQAw_hhdvqg",
 };
