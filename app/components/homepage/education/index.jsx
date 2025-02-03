abdfe1d85f3d45cc44e36cda171cca359cf42a21
hieu <lehieunghiahanh76@gmail.com>
2025-02-03 10:17:08 +0700

    fix

--- app/components/homepage/education/index.jsx
+++ app/components/homepage/education/index.jsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import { educations } from "@/utils/data/educations";
 import { BsPersonWorkspace } from "react-icons/bs";
-// import AnimationLottie from "../../helper/animation-lottie";
+import AnimationLottie from "../../helper/animation-lottie";
 import GlowCard from "../../helper/glow-card";
 import lottieFile from "/public/lottie/study.json";
 
@@ -38,7 +38,7 @@ function Education() {
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-16">
           <div className="flex justify-center items-start">
             <div className="w-3/4 h-3/4">
-              {/* <AnimationLottie animationPath={lottieFile} /> */}
+              <AnimationLottie animationPath={lottieFile} />
             </div>
           </div>
 
